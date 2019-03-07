053dbc2970a83e00888b7ccba769451b62c95805
Víctor Romário <victor.romario.seriado@gmail.com>
2019-03-06 22:21:49 -0300

    Programação dos dois primeiros dias de congresso

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  // Cronograma do primeiro dia
   public dia18manha:Array<object> = [
     {'imagem':'/assets/image/instituto-biologia.jpeg',
      'hora': '09:00 - 12:30',
@@ -50,6 +51,100 @@ export class Tab1Page {
      'participantes': [{'nome':'Marta Bezerra', 'bio': ''}],
      'moderadores': [{'nome':'Marta Bezerra', 'bio':''}]
     }
-  ]
+  ];
+
+  public dia18tarde1416:Array<object> = [
+    {'imagem':'/assets/image/vaolivre-ihac.jpeg',
+     'hora': '14:00 - 16:30',
+     'instituto':'Vão livre do IHAC - Instituto de Humanidades, Artes e Ciências',
+     'titulo':'Teatro do Oprimido',
+     'participantes': [{'nome':'Flávio José Rocha da Silva', 'bio': '- Educação Ambiental e Teatro do Oprimido'}],
+     'moderadores': [{'nome':'Flávio José Rocha da Silva', 'bio':'- Educação Ambiental e Teatro do Oprimido'}]
+    }
+  ];
+
+  public dia18tarde141545:Array<object> = [
+    {'imagem':'/assets/image/vaolivre-ihac.jpeg',
+     'hora': '14:00 - 16:30',
+     'instituto':'Vão livre do IHAC - Instituto de Humanidades, Artes e Ciências',
+     'titulo':'Teatro do Oprimido',
+     'participantes': [{'nome':'Flávio José Rocha da Silva', 'bio': '- Educação Ambiental e Teatro do Oprimido'}],
+     'moderadores': [{'nome':'Flávio José Rocha da Silva', 'bio':'- Educação Ambiental e Teatro do Oprimido'}]
+    },
+    {'imagem':'/assets/image/vaolivre-ihac.jpeg',
+     'hora': '15:45 - 17:15',
+     'instituto':'Auditório A do IGEO  - Instituto de Geociências',
+     'titulo':'Trilhando para uma ecologia política (II): As ações de autodemarcação como luta por reconhecimento e autonomismo',
+     'participantes': [{'nome':'James Angus Fraser', 'bio': '- University of Lancaster, Reino Unido'}, {'nome':'Luiz Felipe Garcia', 'bio':'- Ufopa'}, {'nome':'Rosamaria Loures Ailen Vega', 'bio':'- Berkeley'}, {'nome':'Chico Caititu', 'bio':'- liderança comunitária'}],
+     'moderadores': [{'nome':'Maurício Torres', 'bio':'- UFPA'}, {'nome':'Org. Grupo de Estudos em Ecologia Histórica e Política nas bacias dos rios Trombetas, Tapajós e Xingu', 'bio':''}]
+    },
+    {'imagem':'/assets/image/instituto-biologia.jpeg',
+     'hora': '15:45 - 17:15',
+     'instituto':'Auditório externo do Instituto de Biologia',
+     'titulo':'Chico Mendes vive!',
+     'participantes': [{'nome':'Angela Mendes', 'bio': '- Secretária de Mulheres do Conselho Nacional das Populações Extrativistas, CNS, filha de Chico Mendes'}, {'nome':'Edel Moraes', 'bio':'- Vice-presidenta do Conselho Nacional das Populações Extrativistas, CNS'}, {'nome':'Claudelice Silva Santos', 'bio':'- Estudante de Direito na UNIFESSPA'}, {'nome':'Sonia Guajajara', 'bio':'- Coordenadora da APIB'}],
+     'moderadores': [{'nome':'Org. Edel Moraes', 'bio':''}]
+    },
+    {'imagem':'/assets/image/vaolivre-ihac.jpeg',
+     'hora': '15:45 - 17:15',
+     'instituto':'Auditório Yeda do IGEO  - Instituto de Geociências',
+     'titulo':'El buen vivir en cuestión',
+     'participantes': [{'nome':'Melissa Moreano', 'bio': ''},{'nome':'Nataly Torres', 'bio': ''},{'nome':'Diana Vela Almeida', 'bio': ''},{'nome':'Manuel Bayón', 'bio': ''},{'nome':'Inigo Arrazola', 'bio': ''}],
+     'moderadores': [{'nome':'Org. Colectivo de Geografía Crítica del Ecuador', 'bio':''}]
+    }
+  ];
+
+  public dia18tarde1630:Array<object> = [
+    {'imagem':'/assets/image/vaolivre-ihac.jpeg',
+     'hora': '15:45 - 17:15',
+     'instituto':'Auditório A do IGEO  - Instituto de Geociências',
+     'titulo':'Trilhando para uma ecologia política (II): As ações de autodemarcação como luta por reconhecimento e autonomismo',
+     'participantes': [{'nome':'James Angus Fraser', 'bio': '- University of Lancaster, Reino Unido'}, {'nome':'Luiz Felipe Garcia', 'bio':'- Ufopa'}, {'nome':'Rosamaria Loures Ailen Vega', 'bio':'- Berkeley'}, {'nome':'Chico Caititu', 'bio':'- liderança comunitária'}],
+     'moderadores': [{'nome':'Maurício Torres', 'bio':'- UFPA'}, {'nome':'Org. Grupo de Estudos em Ecologia Histórica e Política nas bacias dos rios Trombetas, Tapajós e Xingu', 'bio':''}]
+    },
+    {'imagem':'/assets/image/instituto-biologia.jpeg',
+     'hora': '15:45 - 17:15',
+     'instituto':'Auditório externo do Instituto de Biologia',
+     'titulo':'Chico Mendes vive!',
+     'participantes': [{'nome':'Angela Mendes', 'bio': '- Secretária de Mulheres do Conselho Nacional das Populações Extrativistas, CNS, filha de Chico Mendes'}, {'nome':'Edel Moraes', 'bio':'- Vice-presidenta do Conselho Nacional das Populações Extrativistas, CNS'}, {'nome':'Claudelice Silva Santos', 'bio':'- Estudante de Direito na UNIFESSPA'}, {'nome':'Sonia Guajajara', 'bio':'- Coordenadora da APIB'}],
+     'moderadores': [{'nome':'Org. Edel Moraes', 'bio':''}]
+    },
+    {'imagem':'/assets/image/vaolivre-ihac.jpeg',
+     'hora': '15:45 - 17:15',
+     'instituto':'Auditório Yeda do IGEO  - Instituto de Geociências',
+     'titulo':'El buen vivir en cuestión',
+     'participantes': [{'nome':'Melissa Moreano', 'bio': ''},{'nome':'Nataly Torres', 'bio': ''},{'nome':'Diana Vela Almeida', 'bio': ''},{'nome':'Manuel Bayón', 'bio': ''},{'nome':'Inigo Arrazola', 'bio': ''}],
+     'moderadores': [{'nome':'Org. Colectivo de Geografía Crítica del Ecuador', 'bio':''}]
+    }
+  ];
+
+  public dia18noite18:Array<object> = [
+    {'imagem':'/assets/image/vaolivre-ihac.jpeg',
+     'hora': '18:00 - 18:30',
+     'instituto':'Salão Nobre da Reitoria da UFBA (Canela)',
+     'titulo':'Insurgências Decoloniais e Horizontes Emancipatórios para o enfrentamento às turbulências atuais',
+     'participantes': [{'nome':'Magnífico Reitor João Carlos Salles ', 'bio': '- UFBA'}],
+     'moderadores': [{'nome':'Magnífico Reitor João Carlos Salles ', 'bio':'- UFBA'}]
+    }
+  ];
+
+  // Cronograma do segundo dia
+  public dia19manha830:Array<object> = [
+    {'imagem':'/assets/image/vaolivre-ihac.jpeg',
+     'hora': '08:30 - 10:15',
+     'instituto':'PAF 5: sala 404',
+     'titulo':'A luta pela manutenção da autonomia comunitária em Juruti Velho, Juruti/PA: o diálogo entre saberes jurídicos acadêmicos e comunitários',
+     'participantes': [{'nome':'Lincon Sousa Aguiar', 'bio': ''},{'nome':'UFOP', 'bio': ''}],
+     'moderadores': [{'nome':'Luiz Enrique Souza', 'bio':''}]
+    },
+    {'imagem':'/assets/image/vaolivre-ihac.jpeg',
+     'hora': '08:30 - 10:15',
+     'instituto':'PAF 5: sala 404',
+     'titulo':'A luta pela manutenção da autonomia comunitária em Juruti Velho, Juruti/PA: o diálogo entre saberes jurídicos acadêmicos e comunitários',
+     'participantes': [{'nome':'Lincon Sousa Aguiar', 'bio': ''},{'nome':'UFOP', 'bio': ''}],
+     'moderadores': [{'nome':'Luiz Enrique Souza', 'bio':''}]
+    }
+  ];
+
 
 }
