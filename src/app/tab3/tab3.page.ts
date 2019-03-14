df20009625244179ed456c13d0adc433bba85859
Víctor Romário <victor.romario.seriado@gmail.com>
2019-03-14 00:01:35 -0300

    Finalização da área de mapas #15

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -9,9 +9,25 @@ import leaflet from 'leaflet';
 export class Tab3Page {
   map: any;
   public center:leaflet.PointTuple;
+  public ibio:leaflet.PointTuple;
+  public igeo:leaflet.PointTuple;
+  public faufba:leaflet.PointTuple;
+  public ihac:leaflet.PointTuple;
+  public paf1:leaflet.PointTuple;
+  public paf3:leaflet.PointTuple;
+  public paf5:leaflet.PointTuple;
+  public vilavelha:leaflet.PointTuple;
  
   constructor() { 
-    this.center= [-29.691484, -53.812409];
+    this.center = [-13.000568540806505, -38.50881973885408];
+    this.ibio = [-13.000797590631436, -38.508692781097125];
+    this.igeo = [-12.998279414804692, -38.50707014393266];
+    this.faufba = [-12.997683687857446, -38.50811680102198];
+    this.ihac = [-13.003146285462813, -38.50967109203339];
+    this.paf1 = [-13.000723873900453, -38.50780705583248];
+    this.paf3 = [-13.002837723143662, -38.50940922918199];
+    this.paf5 = [-13.003838718191286, -38.50945870115538];
+    this.vilavelha = [-12.986501260928508, -38.521125316738114];
   }
  
   ionViewDidEnter(){
@@ -19,22 +35,46 @@ export class Tab3Page {
   }
  
   leafletMap(){
- 
-    this.map=leaflet.map('mapId', {
+    this.map = leaflet.map('mapId', {
        center: this.center,
-       zoom: 17
+       zoom: 16
     });
  
-    //http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
-    //http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}
- 
     var position =leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'NETZ Art Soluções em Internet © LeafLet'
+        attribution: '© LeafLet'
     }).addTo(this.map);
+  
+    var auditorioibio = new leaflet.Marker(this.ibio);
+    var institutogeociencia = new leaflet.Marker(this.igeo);
+    var faculdadearquitetura = new leaflet.Marker(this.faufba);
+    var institutohumanidades = new leaflet.Marker(this.ihac);
+    var predioaulas1 = new leaflet.Marker(this.paf1);
+    var predioaulas3 = new leaflet.Marker(this.paf3);
+    var predioaulas5 = new leaflet.Marker(this.paf5);
+    var teatro = new leaflet.Marker(this.vilavelha);
+    
+    this.map.addLayer(auditorioibio);
+    this.map.addLayer(institutogeociencia);
+    this.map.addLayer(faculdadearquitetura);
+    this.map.addLayer(institutohumanidades);
+    this.map.addLayer(predioaulas1);
+    this.map.addLayer(predioaulas3);
+    this.map.addLayer(predioaulas5);
+    this.map.addLayer(teatro);
  
-    var marker = new leaflet.Marker(this.center);
-    this.map.addLayer(marker);
- 
-    marker.bindPopup("<p> Leaflet Mapa Funcionando. </p>");
+    auditorioibio.bindPopup("<p> Auditório Externo do IBIO </p>");
+    institutogeociencia.bindPopup("<p> IGEO </p>");
+    faculdadearquitetura.bindPopup("<p> Faculdade de Arquitetura </p>");
+    institutohumanidades.bindPopup("<p> IHAC </p>");
+    predioaulas1.bindPopup("<p> Prédio de Aulas 1 </p>");
+    predioaulas3.bindPopup("<p> Prédio de Aulas 1 </p>");
+    predioaulas5.bindPopup("<p> Prédio de Aulas 1 </p>");
+    teatro.bindPopup("<p> Teatro Vila Velha </p>");
+
+    this.map.on('click', function(e) {
+        alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
+    });
+
   }
+
 }
