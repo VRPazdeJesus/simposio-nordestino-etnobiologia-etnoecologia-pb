a2bdecc8ae0195af6391ff90ee4a5520e1eae4f9
Víctor Romário <victor.romario.seriado@gmail.com>
2019-03-14 02:35:42 -0300

    Criação de shapes em volta dos institutos e prédios de aulas

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -52,7 +52,7 @@ export class Tab3Page {
     var predioaulas3 = new leaflet.Marker(this.paf3);
     var predioaulas5 = new leaflet.Marker(this.paf5);
     var teatro = new leaflet.Marker(this.vilavelha);
-    
+
     this.map.addLayer(auditorioibio);
     this.map.addLayer(institutogeociencia);
     this.map.addLayer(faculdadearquitetura);
@@ -67,13 +67,101 @@ export class Tab3Page {
     faculdadearquitetura.bindPopup("<p> Faculdade de Arquitetura </p>");
     institutohumanidades.bindPopup("<p> IHAC </p>");
     predioaulas1.bindPopup("<p> Prédio de Aulas 1 </p>");
-    predioaulas3.bindPopup("<p> Prédio de Aulas 1 </p>");
-    predioaulas5.bindPopup("<p> Prédio de Aulas 1 </p>");
+    predioaulas3.bindPopup("<p> Prédio de Aulas 3 </p>");
+    predioaulas5.bindPopup("<p> Prédio de Aulas 5 </p>");
     teatro.bindPopup("<p> Teatro Vila Velha </p>");
 
-    this.map.on('click', function(e) {
-        alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
-    });
+    var shapeibio = leaflet.polygon([
+        [-13.00073, -38.50886],
+        [-13.00087, -38.50872],
+        [-13.00069, -38.50852],
+        [-13.00056, -38.50866]
+    ]).addTo(this.map);
+
+    var shapepaf1 = leaflet.polygon([
+        [-13.00069, -38.50812],
+        [-13.00094, -38.50786],
+        [-13.00046, -38.50737],
+        [-13.00021, -38.50764]
+    ]).addTo(this.map);
+
+    var shapepaf3 = leaflet.polygon([
+        [-13.00288, -38.50971],
+        [-13.00309, -38.50948],
+        [-13.00274, -38.5091],
+        [-13.00251, -38.50934]
+    ]).addTo(this.map);
+
+    var shapepaf5 = leaflet.polygon([
+        [-13.00362, -38.50956],
+        [-13.0035, -38.50943],
+        [-13.00367, -38.50923],
+        [-13.00375, -38.50932],
+        [-13.00381, -38.50927],
+        [-13.00399, -38.50947],
+        [-13.00393, -38.50953],
+        [-13.00398, -38.50958],
+        [-13.00403, -38.50952],
+        [-13.00409, -38.50958],
+        [-13.00404, -38.50962],
+        [-13.00409, -38.50967],
+        [-13.00396, -38.50979],
+        [-13.00368, -38.50949]
+    ]).addTo(this.map);
+
+    var shapefau = leaflet.polygon([
+        [-12.99748, -38.50851],
+        [-12.99779, -38.50841],
+        [-12.99767, -38.50794],
+        [-12.99802, -38.50785],
+        [-12.99797, -38.50766],
+        [-12.99746, -38.50782],
+        [-12.99758, -38.5083],
+        [-12.99744, -38.50834]
+    ]).addTo(this.map);
+
+    var shapeihac = leaflet.polygon([
+        [-13.00297, -38.50991],
+        [-13.00288, -38.50981],
+        [-13.00324, -38.50944],
+        [-13.00333, -38.50955]
+    ]).addTo(this.map);
+
+    var shapeigeo = leaflet.polygon([
+        [-12.9987, -38.50719],
+        [-12.99857, -38.50674],
+        [-12.99806, -38.50688],
+        [-12.9981, -38.50708],
+        [-12.99806, -38.50709],
+        [-12.99804, -38.50702],
+        [-12.99781, -38.5071],
+        [-12.99782, -38.50717],
+        [-12.99775, -38.50719],
+        [-12.99769, -38.50696],
+        [-12.9975, -38.50705],
+        [-12.99752, -38.50716],
+        [-12.99726, -38.50706],
+        [-12.9972, -38.50723],
+        [-12.99725, -38.50726],
+        [-12.99723, -38.50732],
+        [-12.99745, -38.5074],
+        [-12.99747, -38.50735],
+        [-12.99759, -38.50737],
+        [-12.99762, -38.50747],
+        [-12.99782, -38.50742],
+        [-12.99781, -38.50738],
+        [-12.99789, -38.50736],
+        [-12.9979, -38.5074],
+        [-12.99811, -38.50734],
+        [-12.99809, -38.50727],
+        [-12.99817, -38.50724],
+        [-12.99819, -38.50731],
+        [-12.9984, -38.50725],
+        [-12.99839, -38.5072],
+        [-12.99847, -38.50718],
+        [-12.99848, -38.50724]
+    ]).addTo(this.map);
+
 
   }
 
