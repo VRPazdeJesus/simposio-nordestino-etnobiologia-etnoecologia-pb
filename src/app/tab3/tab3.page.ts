d0adf6e7b92a25f2a9dc094285713777ded14cc0
Víctor Romário <victor.romario.seriado@gmail.com>
2019-03-13 23:05:39 -0300

    Criação da área de mapas com Leaflet #15

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import leaflet from 'leaflet';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  map: any;
+  public center:leaflet.PointTuple;
+ 
+  constructor() { 
+    this.center= [-29.691484, -53.812409];
+  }
+ 
+  ionViewDidEnter(){
+     this.leafletMap();
+  }
+ 
+  leafletMap(){
+ 
+    this.map=leaflet.map('mapId', {
+       center: this.center,
+       zoom: 17
+    });
+ 
+    //http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+    //http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}
+ 
+    var position =leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'NETZ Art Soluções em Internet © LeafLet'
+    }).addTo(this.map);
+ 
+    var marker = new leaflet.Marker(this.center);
+    this.map.addLayer(marker);
+ 
+    marker.bindPopup("<p> Leaflet Mapa Funcionando. </p>");
+  }
+}
