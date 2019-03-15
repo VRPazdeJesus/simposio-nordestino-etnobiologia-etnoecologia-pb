f4a643fcdb8b72232e4cda3ea47a0bf8583986d1
Víctor Romário <victor.romario.seriado@gmail.com>
2019-03-15 19:35:19 -0300

    criação do layout da tela de ebooks como na atividade #8

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  
+  public livros:Array<object> = [
+    {'imagem':'/assets/image/la-selva.png',
+     'titulo': 'La Selva de los Elefantes Blancos',
+     'subtitulo': 'Megaproyectos y extractivismos en la Amazonía ecuatoriana',
+     'autores':'Japhy Wilson e Manuel Bayón',
+     'ano': '',
+     'editora': ''
+    }, 
+    {'imagem':'/assets/image/el-sur.png',
+     'titulo': 'Educação Ambiental Desde El Sur',
+     'subtitulo': '',
+     'autores':'Anne Kassiadou, Celso Sánchez, Daniel Renaud Camargo, Marcelo Aranda Stortti e Rafael Nogueira Costa',
+     'ano': '',
+     'editora': 'Nupem Editora'
+    },
+    {'imagem':'/assets/image/alternativas-sistemicas.png',
+     'titulo': 'Alternativas Sistêmicas',
+     'subtitulo': 'Bem Viver, descrescimento, comuns, ecofeminismo, direitos da Mãe Terra e desglobalização',
+     'autores':'Pablo Solón',
+     'ano': '',
+     'editora': ''
+    }
+  ];
 }
