ea2f86f273a0561f780109250413bc15fbe3b108
Víctor Romário <victor.romario.seriado@gmail.com>
2019-03-16 22:04:02 -0300

    Adicionando a Reitoria da UFBA que não estava sendo localizada #16

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -19,6 +19,7 @@ export class Tab3Page {
   public paf3:leaflet.PointTuple;
   public paf5:leaflet.PointTuple;
   public vilavelha:leaflet.PointTuple;
+  public reitoriaufba:leaflet.PointTuple;
  
   constructor(private geolocation: Geolocation) { 
     this.center = [-13.000568540806505, -38.50881973885408];
@@ -30,6 +31,7 @@ export class Tab3Page {
     this.paf3 = [-13.002837723143662, -38.50940922918199];
     this.paf5 = [-13.003838718191286, -38.50945870115538];
     this.vilavelha = [-12.986501260928508, -38.521125316738114];
+    this.reitoriaufba = [-12.99281, -38.52059];
   }
  
   ionViewDidEnter(){
@@ -54,6 +56,7 @@ export class Tab3Page {
     var predioaulas3 = new leaflet.Marker(this.paf3);
     var predioaulas5 = new leaflet.Marker(this.paf5);
     var teatro = new leaflet.Marker(this.vilavelha);
+    var reitoria = new leaflet.Marker(this.reitoriaufba);
 
     this.map.addLayer(auditorioibio);
     this.map.addLayer(institutogeociencia);
@@ -63,6 +66,7 @@ export class Tab3Page {
     this.map.addLayer(predioaulas3);
     this.map.addLayer(predioaulas5);
     this.map.addLayer(teatro);
+    this.map.addLayer(reitoria);
  
     auditorioibio.bindPopup("<p> Auditório Externo do IBIO </p>");
     institutogeociencia.bindPopup("<p> IGEO </p>");
@@ -72,6 +76,7 @@ export class Tab3Page {
     predioaulas3.bindPopup("<p> PAF 3 </p>");
     predioaulas5.bindPopup("<p> PAF 5 </p>");
     teatro.bindPopup("<p> Teatro Vila Velha </p>");
+    reitoria.bindPopup("<p> Reitoria da UFBA </p>");
 
     var shapeibio = leaflet.polygon([
         [-13.00073, -38.50886],
@@ -162,6 +167,21 @@ export class Tab3Page {
         [-12.99839, -38.5072],
         [-12.99847, -38.50718],
         [-12.99848, -38.50724]
+    ]).addTo(this.map);
+    
+    var shapereitoria = leaflet.polygon([
+        [-12.99263, -38.52079],
+        [-12.99299, -38.52075],
+        [-12.99298, -38.52067],
+        [-12.99305, -38.52063],
+        [-12.99304, -38.52052],
+        [-12.99297, -38.52048],
+        [-12.99296, -38.52039],
+        [-12.99261, -38.52043],
+        [-12.99261, -38.52053],
+        [-12.99257, -38.52053],
+        [-12.99258, -38.5207],
+        [-12.99262, -38.52069]
     ]).addTo(this.map);
     
     //Pegando a localizacao pelo dispositivo
